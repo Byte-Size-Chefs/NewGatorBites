91d836114dc2df39438019cb289c288703b4c45c
Damo <kcolindres@ufl.edu>
2023-10-24 22:29:12 -0400

    Sort by Cuisine and Prompt components updated

--- front-end/src/components/Home/SortByCuisine.tsx
+++ front-end/src/components/Home/SortByCuisine.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const WeeklyPrompt: React.FC = () => {
+const SortByCuisine: React.FC = () => {
   const [prompt, setPrompt] = useState({
     title: "Sort By Cuisine",
     imageUrl: "path-to-spaghetti-image.jpg",
@@ -8,20 +8,21 @@ const WeeklyPrompt: React.FC = () => {
   });
 
   return (
-    <div className="bg-white p-4 rounded-md shadow max-w-sm mx-auto my-4">
-      <h3 className="text-xl font-semibold text-gray-800 mb-2">
-        {prompt.title}
-      </h3>
-
-      <div className="flex justify-center">
-        <img
-          className="rounded-md"
-          src={prompt.imageUrl}
-          alt={prompt.altText}
-        />
+    <div className="bg-white p-4 w-full min-h-screen border border-gray-300 rounded-md shadow-md">
+      <div className="bg-white p-6 w-full max-w-md rounded-md shadow mx-auto">
+        <h3 className="text-2xl font-semibold text-gray-800 mb-4">
+          {prompt.title}
+        </h3>
+        <ul className="space-y-4">
+          <li className="text-gray-800 font-medium">Italian</li>
+          <li className="text-gray-800 font-medium">Mexican</li>
+          <li className="text-gray-800 font-medium">Indian</li>
+          <li className="text-gray-800 font-medium">American</li>
+          <li className="text-gray-800 font-medium">Asian</li>
+        </ul>
       </div>
     </div>
   );
 };
 
-export default WeeklyPrompt;
+export default SortByCuisine;
--- front-end/src/components/Home/WeeklyPrompt.tsx
+++ front-end/src/components/Home/WeeklyPrompt.tsx
@@ -9,13 +9,9 @@ const WeeklyPrompt: React.FC = () => {
   });
 
   return (
-    <div className="bg-white p-4 rounded-md shadow mx-auto my-4 flex flex-col justify-between h-full">
-      <div className="flex flex-col items-center pt-16">
-        {" "}
-        {/* Added padding to move text lower */}
+    <div className="bg-white-900 p-4 rounded-md shadow flex flex-col justify-between w-full h-full">
+      <div className="flex flex-col items-center">
         <h3 className="text-xl font-semibold text-gray-800 mb-2 border-b border-gray-400 pb-2">
-          {" "}
-          {/* Added border bottom */}
           {prompt.title}
         </h3>
         <div className="flex justify-center mt-4">
@@ -25,12 +21,11 @@ const WeeklyPrompt: React.FC = () => {
             alt={prompt.altText}
           />
         </div>
-      </div>
-
-      <div className="flex justify-center mt-4">
-        <button className="bg-green-500 text-black px-4 py-2 rounded-md">
-          Check out the competition
-        </button>
+        <div className="flex justify-center mt-4">
+          <button className="bg-white text-black px-4 py-2 rounded-md">
+            Check out the competition
+          </button>
+        </div>
       </div>
     </div>
   );
