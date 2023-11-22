0efc2203afda53bb8c91ce0a6cea7fb178232d29
Damo <kcolindres@ufl.edu>
2023-11-22 01:09:00 -0500

    Made whole postcards clicable, only title used to be clicable

--- front-end/src/components/Post/PostCard.tsx
+++ front-end/src/components/Post/PostCard.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Post } from '../types/Post';
-import axios from 'axios';
-import authService from '../../services/auth.service';
-import { User } from '../types/User';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Post } from "../types/Post";
+import axios from "axios";
+import authService from "../../services/auth.service";
+import { User } from "../types/User";
 
 export default function PostCard(props: { post: Post; color: string }) {
   const [upvotes, setUpvotes] = useState<number>(props.post.netRating);
@@ -13,50 +13,59 @@ export default function PostCard(props: { post: Post; color: string }) {
     fetchUserDetails();
   }, []);
 
-  useEffect(() => {
-
-    
-  }, [userDetails]); // Run this effect whenever userDetails changes
+  useEffect(() => {}, [userDetails]); // Run this effect whenever userDetails changes
 
   const fetchUserDetails = () => {
-    const username = typeof props.post.user === 'string' ? props.post.user : props.post.user.username;
+    const username =
+      typeof props.post.user === "string"
+        ? props.post.user
+        : props.post.user.username;
 
-    axios.get(`http://localhost:8080/users/${username}`)
+    axios
+      .get(`http://localhost:8080/users/${username}`)
       .then((response) => {
-        console.log('User details from API:', response.data);
+        console.log("User details from API:", response.data);
         setUserDetails(response.data);
-        console.log('User details after setting state:', userDetails);
+        console.log("User details after setting state:", userDetails);
       })
       .catch((error) => {
-        console.error('Error fetching user details:', error);
+        console.error("Error fetching user details:", error);
       });
   };
-  
+
   const clearUpvotes = () => {
     const token = authService.getToken();
     if (token !== null) {
-      const headers = { headers: { 'Authorization': `Bearer ${token}` } };
+      const headers = { headers: { Authorization: `Bearer ${token}` } };
       axios
-        .patch(`http://localhost:8080/user/clearrating/${props.post.id}`, {}, headers)
+        .patch(
+          `http://localhost:8080/user/clearrating/${props.post.id}`,
+          {},
+          headers
+        )
         .then((res) => {
           if (res.status === 200) {
-            console.log('cleared the users upvote');
+            console.log("cleared the users upvote");
             setUpvotes(res.data.data.netRating);
           } else {
-            console.log('failed to clear the users upvote. Not upvoted');
+            console.log("failed to clear the users upvote. Not upvoted");
           }
         });
     } else {
-      console.log('Not logged in');
+      console.log("Not logged in");
     }
   };
 
   const handleUpvote = () => {
     const token = authService.getToken();
     if (token !== null) {
-      const headers = { headers: { 'Authorization': `Bearer ${token}` } };
+      const headers = { headers: { Authorization: `Bearer ${token}` } };
       axios
-        .patch(`http://localhost:8080/user/likepost/${props.post.id}`, {}, headers)
+        .patch(
+          `http://localhost:8080/user/likepost/${props.post.id}`,
+          {},
+          headers
+        )
         .then((res) => {
           if (res.status === 200) {
             setUpvotes(upvotes + 1);
@@ -72,16 +81,20 @@ export default function PostCard(props: { post: Post; color: string }) {
           }
         });
     } else {
-      console.log('Not logged in');
+      console.log("Not logged in");
     }
   };
 
   const handleDownvote = () => {
     const token = authService.getToken();
     if (token !== null) {
-      const headers = { headers: { 'Authorization': `Bearer ${token}` } };
+      const headers = { headers: { Authorization: `Bearer ${token}` } };
       axios
-        .patch(`http://localhost:8080/user/dislikepost/${props.post.id}`, {}, headers)
+        .patch(
+          `http://localhost:8080/user/dislikepost/${props.post.id}`,
+          {},
+          headers
+        )
         .then((res) => {
           if (res.status === 200) {
             setUpvotes(upvotes - 1);
@@ -97,47 +110,68 @@ export default function PostCard(props: { post: Post; color: string }) {
           }
         });
     } else {
-      console.log('Not logged in');
+      console.log("Not logged in");
     }
   };
-  const post_url = '/posts/' + props.post.id;
+  const post_url = "/posts/" + props.post.id;
   console.log(userDetails?.image_url); // Log here
 
   return (
-    <div key={props.post.id} className={`rounded shadow-lg m-2 border-2 ${props.color}`}>
-      <div className='relative m-2'>
-        <div className="flex items-center">
-          {userDetails && ( // Check if userDetails is not null
+    <Link
+      to={post_url}
+      data-cy={`post-${props.post.title}`}
+      style={{ textDecoration: "none" }}
+    >
+      <div
+        key={props.post.id}
+        className={`rounded shadow-lg m-2 border-2 ${props.color}`}
+      >
+        <div className="relative m-2">
+          <div className="flex items-center">
+            {userDetails && ( // Check if userDetails is not null
+              <img
+                src={
+                  userDetails.image_url ||
+                  "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+                }
+                className="rounded-full object-cover w-10 h-10 mr-2"
+                alt="Profile"
+              />
+            )}
+            <div>
+              <p className="url_styling text-lg font-semibold w-5/6">
+                {props.post.title}
+              </p>
+              <span className="text-sm text-gray-500">
+                {" "}
+                made by{" "}
+                {typeof props.post.user === "string"
+                  ? props.post.user
+                  : props.post.user.username}
+              </span>
+            </div>
+          </div>
+
+          <div
+            className="my-3"
+            style={{ width: "100%", maxHeight: "300px", overflow: "hidden" }}
+          >
             <img
-            src={userDetails.image_url || 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'} 
-            className='rounded-full object-cover w-10 h-10 mr-2'
-            alt='Profile'
-          />
-          )}
-          <div>
-            <Link data-cy={`post-${props.post.title}`} to={post_url}>
-              <p className='url_styling text-lg font-semibold w-5/6'>{props.post.title}</p>
-            </Link>
-            <span className='text-sm text-gray-500'> made by {typeof props.post.user === 'string' ? props.post.user : props.post.user.username}</span>
+              src={props.post.imageUrl}
+              className="rounded-md object-cover w-full h-full"
+              alt={props.post.title}
+            />
+          </div>
+
+          {/* The rest of your existing code remains unchanged */}
+
+          <div className="absolute top-0 right-0">
+            {upvotes}
+            <button onClick={() => handleUpvote()}>⬆</button>
+            <button onClick={() => handleDownvote()}>⬇</button>
           </div>
         </div>
-
-        <div className='my-3' style={{ width: '100%', maxHeight: '300px', overflow: 'hidden' }}>
-          <img
-            src={props.post.imageUrl}
-            className='rounded-md object-cover w-full h-full'
-            alt={props.post.title}
-          />
-        </div>
-
-        {/* The rest of your existing code remains unchanged */}
-
-        <div className='absolute top-0 right-0'>
-          {upvotes}
-          <button onClick={() => handleUpvote()}>⬆</button>
-          <button onClick={() => handleDownvote()}>⬇</button>
-        </div>
       </div>
-    </div>
+    </Link>
   );
 }
